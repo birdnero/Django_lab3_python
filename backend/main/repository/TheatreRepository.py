d8babf1fb592e0a0bc69fe1a2bcd6992b88bfb69
birdnero <neroforwork@gmail.com>
2025-12-17 15:36:08 +0200

    added a few endpoints for olena

--- backend/main/repository/TheatreRepository.py
+++ backend/main/repository/TheatreRepository.py
@@ -10,3 +10,12 @@ class TheatreRepository(BaseRepository):
 
     def rating(self):
         return self.model.objects.annotate(rating=Avg("hall__schedule__play__playrating__rating")).order_by("-rating")
+
+    def daily_tickets(self):
+        return (
+            self.model.objects.filter(hall__schedule__ticket__status="проданий")
+            .values("name", date=F("hall__schedule__date"))
+            .annotate(
+                income=Sum("hall__schedule__ticket__price"), tickets_sold=Count("hall__schedule__ticket__ticket_id")
+            )
+        )
