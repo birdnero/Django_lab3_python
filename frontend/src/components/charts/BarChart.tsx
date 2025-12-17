61b783b6fe99e038695834e901f9e096d4fa107c
birdnero <neroforwork@gmail.com>
2025-12-17 16:00:01 +0200

    make example of fixed login issue (see barchart)

--- frontend/src/components/charts/BarChart.tsx
+++ frontend/src/components/charts/BarChart.tsx
@@ -9,7 +9,7 @@ import {
 } from "recharts";
 import { useEffect, useState } from "react";
 import { Varialbles } from "../../config";
-import { useToken } from "../../utils/StateManager";
+import { getQuery } from "../../utils/RestUtils";
 
 type RatingItem = {
   name: string;
@@ -21,18 +21,11 @@ export default function MyBarChart() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`${Varialbles.backend}api/theaters/stats/rating/2/`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${useToken.getState().token}`,
-      },
-    })
-      .then((res) => res.json())
+    getQuery(`${Varialbles.backend}api/theaters/stats/rating/2/`)
       .then((data) => {
-        setData(data);
+        setData(data as RatingItem[] ?? []);
+        setLoading(false)
       })
-      .finally(() => setLoading(false));
   }, []);
 
   if (loading) return <div>Loading...</div>;
